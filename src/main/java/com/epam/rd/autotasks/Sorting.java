27ec4c153fc88f18c0976dd28aaf890fbae2f85a
Nino Kereselidze <ninokereselidze30@gmail.com>
2023-09-18 21:38:11 +0400

    Testing Sorting Routine

--- src/main/java/com/epam/rd/autotasks/Sorting.java
+++ src/main/java/com/epam/rd/autotasks/Sorting.java
@@ -1,7 +1,11 @@
 package com.epam.rd.autotasks;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Sorting {
-    public void sort(int[] array){
-        throw new UnsupportedOperationException();
+    public void sort(int[] array) {
+        if(Objects.isNull(array)) throw new IllegalArgumentException();
+        Arrays.sort(array);
     }
 }
--- src/main/java/com/epam/rd/autotasks/SortingTest.java
+++ src/main/java/com/epam/rd/autotasks/SortingTest.java
@@ -2,34 +2,57 @@ package com.epam.rd.autotasks;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 
 public class SortingTest {
 
     Sorting sorting = new Sorting();
 
-    @Test
+    @Test(expected =  IllegalArgumentException.class)
     public void testNullCase(){
-
+        int[] expected = null;
+        sorting.sort(expected);
     }
 
     @Test
     public void testEmptyCase(){
-
+        int[] expected = {};
+        int expectedSize = 0;
+        sorting.sort(expected);
+        Assert.assertEquals(expected.length, expectedSize);
+        Exception ex = null;
+        try{
+            sorting.sort(expected);
+        }catch (Exception e){
+            ex = e;
+        }
+        Assert.assertNull(ex);
     }
 
     @Test
     public void testSingleElementArrayCase() {
-
+        int[] expected = {4};
+        int expectedElement = expected[0];
+        int expectedLength = 1;
+        sorting.sort(expected);
+        Assert.assertEquals(expected.length, expectedLength);
+        Assert.assertEquals(expectedElement, expected[0]);
     }
 
     @Test
     public void testSortedArraysCase() {
-
+        int[] actual = {5, 7, 9, 13, 27, 30};
+        int [] expected = Arrays.copyOf(actual,actual.length);
+        sorting.sort(actual);
+        Assert.assertArrayEquals(expected, actual);
     }
 
     @Test
     public void testOtherCases() {
-
+        int[] sampleArray = {3, 6, 1, 15, 5, 23, 24, 64, 45, 90, 3};
+        int[] sortedArray = Arrays.copyOf(sampleArray,sampleArray.length);
+        Arrays.sort(sortedArray);
+        sorting.sort(sampleArray);
+        Assert.assertArrayEquals(sortedArray, sampleArray);
     }
-
 }
